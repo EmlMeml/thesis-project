7d44163c2c765ad8ea33a02ea7d06f01a7f52ca1
Melanie <135548773+EmlMeml@users.noreply.github.com>
2026-05-18 08:54:38 +0200

    added placeholder

--- src/custom/advancedEditor.js
+++ src/custom/advancedEditor.js
@@ -96,7 +96,7 @@ function TextEditor({ editor }) {
     </IconButton>
 
   </div>
-  <Editable className="editorEditable" onKeyDown={onKeyDown} renderLeaf={renderLeaf} />
+  <Editable className="editorEditable" onKeyDown={onKeyDown} renderLeaf={renderLeaf} placeholder="Begin your Story..."/>
 </div>;
 }
 
