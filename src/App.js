6ac3c60bbb622cf5120879aee068465ec0e31897
Melanie <135548773+EmlMeml@users.noreply.github.com>
2026-06-08 14:19:26 +0200

    base chat visual

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import React from 'react';
 import TopBar from './custom/TopBar.js';
 import { MyEditor } from './custom/Editor.tsx';
+import { MyChat } from './custom/Chat.tsx';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <div id="editor-container">
-      <MyEditor />
+      <div id="main-content">
+        <div id="editor-container">
+          <MyEditor />
+        </div>
+        <MyChat />
       </div>
       
-      <div>
-        <br></br>
-        <br></br>
-        <br></br>
-      </div>
+      
     </div>
     
   );
