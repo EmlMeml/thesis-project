ce14b6f1e8493734697e4a2780c78af160ef7dad
Melanie <135548773+EmlMeml@users.noreply.github.com>
2026-06-18 09:51:29 +0200

    basic input for file

--- src/custom/TopBar.js
+++ src/custom/TopBar.js
@@ -1,8 +1,11 @@
+import FileUploader from './../custom/FileUploader.tsx';
+
 export default function TopBar() {
 
     return(
         <div style={{ backgroundColor: '#252e5b', color: '#fff', height:'56px', alignItems:'center', display:'flex', justifyContent:'center' }}>
             <h1>Tell Me a Story...</h1>
+            <FileUploader />
         </div>
     )
 
