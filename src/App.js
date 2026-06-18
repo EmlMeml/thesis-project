f188eea8daf2edbf13ad9cf08cc4c07eb7ad733a
Melanie <135548773+EmlMeml@users.noreply.github.com>
2026-06-18 10:18:05 +0200

    file upload into editor works

--- src/App.js
+++ src/App.js
@@ -7,16 +7,37 @@ import { MyChat } from './custom/Chat.tsx';
 import { TextPreview } from './custom/TextPreview.tsx';
 import { TextNav } from './custom/TextNav.tsx';
 
+function textToSlateValue(text) {
+  if (!text) {
+    return [
+      {
+        type: 'paragraph',
+        children: [{ text: '' }],
+      },
+    ];
+  }
+
+  return text.split(/\r?\n/).map((line) => ({
+    type: 'paragraph',
+    children: [{ text: line }],
+  }));
+}
+
 function App() {
   const [editorContent, setEditorContent] = useState([]);
+  const [fileText, setFileText] = useState('');
+
+  const handleFileLoad = (text) => {
+    setFileText(text);
+  };
 
   return (
     <div className="App">
-      <TopBar />
+      <TopBar onFileLoad={handleFileLoad} />
       <div id="main-content">
-        <TextNav /> 
+        <TextNav />
         <div id="editor-container">
-          <MyEditor onContentChange={setEditorContent} />
+          <MyEditor fileText={fileText} onContentChange={setEditorContent} />
         </div>
         <div id="animation-container">
           <TextPreview content={editorContent} />
