aeb8137e31d04806c2fa6340f14ea31955b8d835
Melanie <135548773+EmlMeml@users.noreply.github.com>
2026-06-18 09:59:01 +0200

    basic function for fileuploader

--- src/custom/FileUploader.tsx
+++ src/custom/FileUploader.tsx
@@ -1,5 +1,17 @@
+import React, { ChangeEvent, useState } from 'react';
+
 export default function FileUploader() {
-        return <div>
-            <input type="file" id="file-upload"/>
+        
+    const [file, setFile] = useState<File | null>(null);
+    
+    function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+        if(e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    }
+
+    return <div>
+            <input type="file" id="file-upload" onChange={handleFileChange}/>
+            {file && <p>Selected file: {file.name}</p>}
         </div>;
     }
