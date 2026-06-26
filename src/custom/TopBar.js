a280b952ed549709701eca22c19336e3342da6e3
Melanie <135548773+EmlMeml@users.noreply.github.com>
2026-06-26 13:37:45 +0200

    topbar change

--- src/custom/TopBar.js
+++ src/custom/TopBar.js
@@ -3,7 +3,7 @@ import FileUploader from './../custom/FileUploader.tsx';
 export default function TopBar({ onFileLoad }) {
   return (
     <div style={{ backgroundColor: '#252e5b', color: '#fff', height:'56px', alignItems:'center', display:'flex', justifyContent:'center' }}>
-      <h1>Tell Me a Story...</h1>
+      <h1>Titel</h1>
       <div style={{ position: 'absolute', right: '16px'}}>
         <FileUploader onTextLoad={onFileLoad} />
       </div>
