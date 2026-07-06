fe8ca2875c0f382ec4d31610edcf78e2cc941976
Melanie <135548773+EmlMeml@users.noreply.github.com>
2026-07-06 11:01:32 +0200

    show Stone Types

--- src/custom/ChangeCreator.tsx
+++ src/custom/ChangeCreator.tsx
@@ -1,10 +1,19 @@
+import React, { ChangeEvent, useState } from 'react';
+
+const intensityMapping = ['Sand', 'Gravel', 'Stone', 'Cobblestone', 'Rock'];
 export const ChangeCreator = () => {
+    const [intensityIndex, setIntensityIndex] = useState(2); // Default to "Stone"
+
+    const handleIntensityChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setIntensityIndex(Number(e.target.value));
+    };
+
   return (
     <div id="change-creator" className="change-creator">
-        <h3>Create your Pebble</h3>
-        <div id="change-description-container">
+        <h3>Create your Change</h3>
+        <div id="change-description-container" className="change-elements">
             <p>Describe the changes you want to make:</p>
-            <textarea id="change-description" className="change-elements" placeholder="Enter your change description here..." rows={5}></textarea>
+            <textarea id="change-description" placeholder="Enter your change description here..." rows={5}></textarea>
         </div>
         <div id="change-scope-container" className="change-elements">
             <p>Describe the scope of the changes:</p>
@@ -17,7 +26,16 @@ export const ChangeCreator = () => {
        </div>
         <div id="change-intensity-container" className="change-elements">
             <p>Describe the intensity of the changes:</p>
-            <input type="range" id="change-intensity"  min="1" max="5" defaultValue="3" />
+            <input 
+                type="range" 
+                id="change-intensity"  
+                min={0} 
+                max={intensityMapping.length - 1} 
+                step={1}
+                value={intensityIndex}
+                onChange={(e) => setIntensityIndex(Number(e.target.value))}
+            />
+            <output id="change-intensity-output">{intensityMapping[intensityIndex]}</output>
         </div>
        <div id="change-visualize-container">
        {/* Todo: Add dynamic pic for stone that is "thrown" based on the intensity of the change */}
